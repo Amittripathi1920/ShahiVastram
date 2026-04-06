3982defc847d412321573a5b2a14050eebaa7a7c
Amit Tripathi <amit5000tripathi@gmail.com>
2026-04-06 22:11:17 +0530

    fixed env var v5

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,7 +2,7 @@ export const env = {
   appUrl: process.env.NEXT_PUBLIC_APP_URL,
   supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
   supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  razorpayKeyId: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID
+  razorpayKeyId: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
   adminJwtSecret: process.env.ADMIN_JWT_SECRET ?? "change-me-in-env",
 };
 
